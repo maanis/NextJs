20f0983c9280ea8b891476e8ec704d6f08efc29b
maanis <manishjha962@gmail.com>
2025-06-19 22:56:01 +0530

    commited prisma

--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,21 +1,39 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
+import { prisma } from '@/lib/prisma'
 import { Label } from '@radix-ui/react-label'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Snippet = () => {
 
-    
+    const createSnippet = async (formData: FormData) => {
+        "use server"
+        const title = formData.get('title') as string
+        const code = formData.get('code') as string
+
+        const snippet = await prisma.snippet.create({
+            data: {
+                title, code
+            }
+        })
+
+        console.log(snippet)
+        redirect('/')
+
+
+    }
+
     return (
-        <form className='space-y-3'>
+        <form action={createSnippet} className='space-y-3'>
             <div>
                 <Label>Title</Label>
-                <Input type='text' />
+                <Input type='text' name='title' id='title' />
             </div>
             <div>
                 <Label>Code</Label>
-                <Textarea></Textarea>
+                <Textarea name='code' id='code'></Textarea>
             </div>
             <Button type='submit'>Create</Button>
         </form>
